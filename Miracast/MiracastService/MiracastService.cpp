6c8bb38b8614dc87a4b67954f8f22f0ce4201d8f
Yuvaramachandran Gurusamy <123441336+yuvaramachandran-gurusamy@users.noreply.github.com>
2025-05-22 19:38:05 +0530

    RDKEMW-3285: Fixed the discovery sync issue (#60)
    * RDKEMW-3285: Fixed the discovery sync issue
    
    Signed-off-by: yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>
    
    * RDKEMW-4522: Use udhcpc.script from /etc/wifi_p2p Miracast P2P Client Mode
    
    Reason for change: Earlier p2p_udhcpc script available in netsrvmgr which is deprecated. Hence moving it to here.
    Test Procedure: Miracast should work in P2P client mode
    Risks: None
    Priority: P1
    
    Signed-off-by: yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>
    
    ---------
    
    Signed-off-by: yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>

--- Miracast/MiracastService/MiracastService.cpp
+++ Miracast/MiracastService/MiracastService.cpp
@@ -432,6 +432,7 @@ namespace WPEFramework
 					if (!m_isServiceEnabled)
 					{
 						lock_guard<recursive_mutex> lock(m_EventMutex);
+						m_isServiceEnabled = true;
 						if (m_IsTransitionFromDeepSleep)
 						{
 							LOGINFO("#### MCAST-TRIAGE-OK Enable Miracast discovery Async");
@@ -442,7 +443,6 @@ namespace WPEFramework
 						{
 							setEnable(true);
 						}
-						m_isServiceEnabled = true;
 						response["message"] = "Successfully enabled the WFD Discovery";
 						success = true;
 					}
@@ -460,6 +460,7 @@ namespace WPEFramework
 					else if (m_isServiceEnabled)
 					{
 						lock_guard<recursive_mutex> lock(m_EventMutex);
+						m_isServiceEnabled = false;
 						if (!m_IsTransitionFromDeepSleep)
 						{
 							if ( MIRACAST_SERVICE_STATE_RESTARTING_SESSION == current_state )
@@ -475,7 +476,6 @@ namespace WPEFramework
 						{
 							LOGINFO("#### MCAST-TRIAGE-OK Skipping Disable discovery as done by PwrMgr");
 						}
-						m_isServiceEnabled = false;
 						remove_wifi_connection_state_timer();
 						remove_miracast_connection_timer();
 						response["message"] = "Successfully disabled the WFD Discovery";
@@ -1499,7 +1499,15 @@ namespace WPEFramework
 				case MIRACAST_SERVICE_STATE_IDLE:
 				case MIRACAST_SERVICE_STATE_DISCOVERABLE:
 				{
-					changeServiceState(state);
+					if ((!m_isServiceEnabled) && (MIRACAST_SERVICE_STATE_DISCOVERABLE == state))
+					{
+						/*User already disabled the discovery, so should not enable again.*/
+						m_miracast_ctrler_obj->stop_discoveryAsync();
+					}
+					else
+					{
+						changeServiceState(state);
+					}
 				}
 				break;
 				default:
