86ec4336bf8e78852b0e4b31d1f0b4c53b7b540f
agampa263 <133222558+agampa263@users.noreply.github.com>
2025-05-27 10:09:14 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -31,6 +31,9 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-casting \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/rdk/iarmmgrs-hal \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/ccec/drivers \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/network \
+-I${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks \
+-I${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/thunder \
+-I${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/devicesettings \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/devicesettings.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/Iarm.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/Rfc.h \
