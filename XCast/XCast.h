a28310e35cf02443f868d04cb91f17b539969d16
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-06-24 00:39:24 +0530

    Update XCast.h

--- XCast/XCast.h
+++ XCast/XCast.h
@@ -31,7 +31,7 @@
 namespace WPEFramework {
 
     namespace Plugin {
-		
+			
 		class XCast : public PluginHost::IPlugin, public PluginHost::JSONRPC
 		{
 			private:
@@ -57,14 +57,58 @@ namespace WPEFramework {
 						INTERFACE_ENTRY(Exchange::IXCast::INotification)
 						INTERFACE_ENTRY(RPC::IRemoteConnection::INotification)
 						END_INTERFACE_MAP
+
+						 virtual void OnApplicationLaunchRequestWithLaunchParam(const string& appName, const string& strPayLoad, const string& strQuery, const string& strAddDataUrl) override
+						{
+							LOGINFO("OnApplicationLaunchRequestWithLaunchParam: appName=%s, strPayLoad=%s, strQuery=%s, strAddDataUrl=%s",
+								appName.c_str(), strPayLoad.c_str(), strQuery.c_str(), strAddDataUrl.c_str());
+							Exchange::JXCast::Event::OnApplicationLaunchRequestWithLaunchParam(_parent, appName, strPayLoad, strQuery, strAddDataUrl);
+						}
+						virtual void OnApplicationLaunchRequest(const string& appName, const string& parameter) override
+						{
+							LOGINFO("OnApplicationLaunchRequest: appName=%s, parameter=%s", appName.c_str(), parameter.c_str());
+							Exchange::JXCast::Event::OnApplicationLaunchRequest(_parent, appName, parameter);
+						}
+						virtual void OnApplicationStopRequest(const string& appName, const string& appID) override
+						{
+							LOGINFO("OnApplicationStopRequest: appName=%s, appID=%s", appName.c_str(), appID.c_str());
+							Exchange::JXCast::Event::OnApplicationStopRequest(_parent, appName, appID);
+						}
+						virtual void OnApplicationHideRequest(const string& appName, const string& appID) override
+						{
+							LOGINFO("OnApplicationHideRequest: appName=%s, appID=%s", appName.c_str(), appID.c_str());
+							Exchange::JXCast::Event::OnApplicationHideRequest(_parent, appName, appID);
+						}
+						virtual void OnApplicationStateRequest(const string& appName, const string& appID) override
+						{
+							LOGINFO("OnApplicationStateRequest: appName=%s, appID=%s", appName.c_str(), appID.c_str());
+							Exchange::JXCast::Event::OnApplicationStateRequest(_parent, appName, appID);
+						}
+						virtual void OnApplicationResumeRequest(const string& appName, const string& appID) override
+						{
+							LOGINFO("OnApplicationResumeRequest: appName=%s, appID=%s", appName.c_str(), appID.c_str());
+							Exchange::JXCast::Event::OnApplicationResumeRequest(_parent, appName, appID);
+						}
 							
-						void Activated(RPC::IRemoteConnection *) final
+						void Activated(RPC::IRemoteConnection *connection) final
 						{
 							LOGINFO("XCast Notification Activated");
+							// if (connection->Id() == _parent._connectionId)
+            				// {
+							// 	LOGINFO("XCast Notification Activated");
+            				//     ASSERT(nullptr != _parent._service);
+            				//     Core::IWorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_parent._service, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::FAILURE));
+            				// }
 						}
 		
 						void Deactivated(RPC::IRemoteConnection *connection) final
 						{
+							//  if (connection->Id() == _parent._connectionId)
+            				// {
+							// 	LOGINFO("XCast Notification Deactivated");
+            				//     ASSERT(nullptr != _parent._service);
+            				//     Core::IWorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_parent._service, PluginHost::IShell::DEACTIVATED, PluginHost::IShell::FAILURE));
+            				// }
 							LOGINFO("XCast Notification Deactivated");
 							_parent.Deactivated(connection);
 						}
@@ -100,7 +144,9 @@ namespace WPEFramework {
 					uint32_t _connectionId{};
 					Exchange::IXCast *_xcast{};
 					Core::Sink<Notification> _xcastNotification;
-					Exchange::IConfiguration* configure;
+					//Exchange::IConfiguration* configure;
+
+					friend class Notification;
         };
 
     } // namespace Plugin
